08ddf092ce465e870dc3cd2dc778f9dace05b774
AndreyMikhailov <mands315@gmail.com>
2023-05-29 22:37:50 +0300

    Change NavBar bg color

--- Todoey/View/CustomView.swift
+++ Todoey/View/CustomView.swift
@@ -11,7 +11,7 @@ class CustomView: UIView {
     
     private lazy var bgNavBarImageView: UIImageView = {
         let element = UIImageView()
-        element.backgroundColor = .systemBlue
+        element.backgroundColor = .systemCyan
         element.translatesAutoresizingMaskIntoConstraints = false
         return element
     }()
